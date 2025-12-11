6c62ab24d5e198d75fe38ccbda891d78caf7c10d
Jeral Pineda <jeral.cris@gmail.com>
2025-12-10 21:59:57 -0600

    feat: add types for data

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export type NavLink = {
+  id: string;
+  title: string;
+};
+
+export type CocktailList = {
+  name: string;
+  country: string;
+  detail: string;
+  price: string;
+};
+
+export type ProfileList = {
+  imgPath: string;
+};
+
+export type StoreInfo = {
+  heading: string;
+  address: string;
+  contact: {
+    phone: string;
+    email: string;
+  };
+};
+
+export type OpeningHour = { day: string; time: string };
+
+export interface Social {
+  name: string;
+  icon: string;
+  url: string;
+}
+
+export interface SliderItem {
+  id: number;
+  name: string;
+  image: string;
+  title: string;
+  description: string;
+}
