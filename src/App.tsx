f395f38bfa8d330ce5f7e4698986eca6e4ccb5d3
Jeral Pineda <jeral.cris@gmail.com>
2025-12-10 22:00:51 -0600

    feat: implement navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import { Navbar } from "./components/navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl font-bold underline">App</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 }
 
