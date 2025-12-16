427a72a1524960450d40f7c85a489faa2834b2a9
Jeral Pineda <jeral.cris@gmail.com>
2025-12-15 20:53:53 -0600

    fix: upgrade navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,21 +4,21 @@ import gsap from "gsap";
 
 export const Navbar = () => {
   useGSAP(() => {
-    const navTween = gsap.timeline({
-      scrollTrigger: {
-        trigger: "nav",
-        start: "bottom top",
-      },
-    });
-
-    navTween.fromTo(
+    gsap.fromTo(
       "nav",
-      { backgroundColor: "transparent" },
       {
-        backgroundColor: "#00000050",
-        backgroundFilter: "blur(10px)",
+        backgroundColor: "transparent",
+      },
+      {
+        backgroundColor: "rgba(0,0,0,0.3)",
+        backdropFilter: "blur(10px)",
         duration: 1,
         ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: "nav",
+          start: "bottom top",
+          toggleActions: "play none none reverse",
+        },
       }
     );
   }, []);
