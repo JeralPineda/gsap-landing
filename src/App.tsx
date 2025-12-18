9b4bdea47285da43b2092078e171cb290b0e0064
Jeral Pineda <jeral.cris@gmail.com>
2025-12-17 19:37:16 -0600

    fix: Implement the about section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import { Hero } from "./components/hero";
 import { Navbar } from "./components/navbar";
 import { Cocktails } from "./components/cocktails";
+import { About } from "./components/about";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 }
